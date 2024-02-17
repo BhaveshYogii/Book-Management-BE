92e76cb3ab5f85466cd9099154feacb3797c136d
Devanshee-Gupta <devansheegupta0@gmail.com>
2024-02-17 06:55:31 +0530

    pipeline added

--- backend/scripts/before_install.sh
+++ backend/scripts/before_install.sh
@@ -2,6 +2,9 @@
 
 
 # update os & install python3 and libpq-dev for postgresql
+
+
+cd home/ubuntu/Team3-Book-Management-BE
 sudo apt-get update
 sudo apt-get install -y python3 python3-dev python3-pip python3-venv libpq-dev
 pip install --user --upgrade virtualenv
